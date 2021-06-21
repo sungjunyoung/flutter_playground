245cb3f0f96ec3663a5276e1d888a171787bf375
Junyoung, Sung <junyoung.sung@navercorp.com>
2021-06-21 23:49:32 +0900

    Initial commit with camera screen

---
+++ lib/Screens/Home/HomeScreen.dart
@@ -0,0 +1,25 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_playground/Screens/Home/CameraScreen.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        CupertinoButton.filled(
+          child: Text('카메라 테스트'),
+          onPressed: () async {
+            var cameras = await availableCameras();
+            Get.to(() => CameraScreen(), arguments: cameras);
+          },
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_playground/Screens/Home/HomeScreen.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Startup Name Generator',
+      darkTheme: ThemeData.dark(),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Playground'),
+          ),
+          body: HomeScreen()),
+    );
+  }
+}
